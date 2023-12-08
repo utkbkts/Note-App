879ef5e2c76b1b0cbfcc0889194f97c1161c3ded
Utku Bektasoglu <utkbktss5@gmail.com>
2023-12-08 23:54:33 +0300

    update Button

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import theme from "./theme/Theme.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  <ChakraProvider>
     <BrowserRouter>
-      <App />
+        <App />
     </BrowserRouter>
   </ChakraProvider>
 );
